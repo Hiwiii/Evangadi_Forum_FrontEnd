fa509d97e7defe6453993ddcfdf0b4ffd5eb5e90
Hiwiii <42866660+Hiwiii@users.noreply.github.com>
2024-08-13 14:30:27 +0300

    edited the connection url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,9 @@
 
 import axios from "axios";
 const axiosBase = axios.create({
-    baseURL: "http://localhost:5400/api",
+    // baseURL: "http://localhost:5400/api",
+    baseURL: "https://evangadi-forum-ce9a2f3ee76f.herokuapp.com/api",
+
 });
 
 export default axiosBase;
